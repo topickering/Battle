2842d953a4eefeb4fb99ebb7286a9d45284d1e2d
topickering <topickering@gmail.com>
2018-11-14 12:45:14 +0000

    Sessions enabled

--- app.rb
+++ app.rb
@@ -2,16 +2,24 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    p params
-    @player_1_name = params[:Player_1_Name]
-    @player_2_name = params[:Player_2_Name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb(:play)
   end
+
 
   run! if app_file == $0
 
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,8 +1,8 @@
 feature 'Name entry' do
   scenario 'players enter names' do
     visit('/')
-    fill_in "Player_1_Name", with: "A Name"
-    fill_in "Player_2_Name", with: "Another Name"
+    fill_in "player_1_name", with: "A Name"
+    fill_in "player_2_name", with: "Another Name"
     click_button 'Submit'
     expect(page).to have_content "A Name"
     expect(page).to have_content "Another Name"
