80641f5886e4dcfed7d08e9f294daa4f08547ad2
topickering <topickering@gmail.com>
2018-11-14 14:28:04 +0000

    Commit: added test helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Name entry' do
   scenario 'players enter names' do
-    visit('/')
-    fill_in "player_1_name", with: "A Name"
-    fill_in "player_2_name", with: "Another Name"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "A Name"
     expect(page).to have_content "Another Name"
   end
